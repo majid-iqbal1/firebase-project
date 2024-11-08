7685e1f0af1547b68575cdca8b3b3a1e60b32e61
Majid Iqbal <majidi2@uw.edu>
2024-11-08 06:21:11 -0800

    created .env file for encrytion

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,14 +3,16 @@ import {getAuth, onAuthStateChanged, GoogleAuthProvider} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
 
-const app = initializeApp({
-    apiKey: "AIzaSyA80DFHsG5MvaOIaPBAXRJIn8OqVo8-TQI",
-  authDomain: "study-group-36edd.firebaseapp.com",
-  projectId: "study-group-36edd",
-  storageBucket: "study-group-36edd.firebasestorage.app",
-  messagingSenderId: "855896027198",
-  appId: "1:855896027198:web:27a8bad3a656894cdd216f"
-});
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+  };
+
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
